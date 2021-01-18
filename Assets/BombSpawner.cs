b189527eb4a30fcbe9ec77f4c0f52ccf2973a910
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2021-01-18 17:26:18 +0100

    Created movement for the character

---
+++ Assets/BombSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public GameObject bombPrefab;
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Vector3Int cell = tilemap.WorldToCell(player.transform.position);
+            Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        }
+    }
+}
